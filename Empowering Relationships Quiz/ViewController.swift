b763e8c8ecb8958cfd231f82e808967b0e580252
TIffany Kong <tkon0005@student.monash.edu>
2020-09-18 02:22:21 +1000

    Initial Commit

---
+++ Empowering Relationships Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Empowering Relationships Quiz
+//
+//  Created by Tiffany Kong on 18/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Empowering Relationships QuizTests/Empowering_Relationships_QuizTests.swift
@@ -0,0 +1,33 @@
+//
+//  Empowering_Relationships_QuizTests.swift
+//  Empowering Relationships QuizTests
+//
+//  Created by Tiffany Kong on 18/9/20.
+//
+
+import XCTest
+@testable import Empowering_Relationships_Quiz
+
+class Empowering_Relationships_QuizTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
